dd170e32aa40f7b0ed91add4d737ea78c8ec724a
Connor McCormack <mccormack.connor.m@gmail.com>
2020-03-22 21:09:25 -0500

    on refresh, added items should stay

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
 import bookmarkList from './bookmarks-list.js';
 import store from './store.js';
-// import api from './api.js';
+import api from './api.js';
 
 const main = function () {
-  
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) =>  store.addItem(item));
+    });
   
   bookmarkList.bindEventListeners();
   bookmarkList.render();
